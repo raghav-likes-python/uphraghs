29ffbb01a4faa63d7ae46cecdaf9b52392e91095
Raghav <168405183+raghav-likes-python@users.noreply.github.com>
2024-11-28 14:27:53 +0530

    Update script.js
    Js god

--- games/strs/script.js
+++ games/strs/script.js
@@ -1,3 +1,4 @@
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
